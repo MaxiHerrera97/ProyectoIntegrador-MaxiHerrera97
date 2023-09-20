ad89ae640cc5d91ca0318ca1ec1960bed9269381
MaxiHerrera97 <maximilianogabriel0511@gmail.com>
2023-09-20 20:00:02 -0300

    [ADD] components

--- front/src/components/card/Card.jsx
+++ front/src/components/card/Card.jsx
@@ -1,7 +1,8 @@
-const Card =({name, status, origin, species, gender, image, onClose}) => {
+
+const Card =({ id, name, status, origin, species, gender, image, onClose}) => {
    return (
       <div>
-          <button onClick={onClose}>X</button>
+          <button onClick={() => onClose(id)}>X</button>
          <h2>{name}</h2>
          <h2>{species}</h2>
          <h2>{gender}</h2>
---
+++ front/src/components/nav/Nav.jsx
@@ -0,0 +1,11 @@
+import SearchBar from "../searchbar/SearchBar";
+
+const Nav = ({onSearch})=>{
+    return (
+        <nav>
+            <SearchBar onSearch={onSearch}/>
+        </nav>
+    )
+}
+
+export default Nav;
